152f2b1de74e2f3910dd481756bfcc8565f54f5a
Josh Bernfeld <joshbernfeld@gmail.com>
2022-02-10 20:15:21 -0800

    Fixed issue where editing media of certain sizes would cause the UI to freeze (e.g. selfies from the native Camera app)
    General bug fixes and stability improvements

--- Example/Pods/Target Support Files/PixelSDK-GPUImage3/PixelSDK-GPUImage3-xcframeworks.sh
+++ Example/Pods/Target Support Files/PixelSDK-GPUImage3/PixelSDK-GPUImage3-xcframeworks.sh
@@ -105,5 +105,5 @@ install_xcframework() {
   echo "Copied $source to $destination"
 }
 
-install_xcframework "${PODS_ROOT}/PixelSDK-GPUImage3/framework/Build/GPUImage.xcframework" "PixelSDK-GPUImage3" "framework" "ios-arm64_x86_64-simulator" "ios-arm64"
+install_xcframework "${PODS_ROOT}/PixelSDK-GPUImage3/framework/Build/GPUImage.xcframework" "PixelSDK-GPUImage3" "framework" "ios-arm64" "ios-arm64_x86_64-simulator"
 
